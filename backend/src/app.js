3ae0817881f605925a584c78ef3b04f2caac4602
Tejasshelke1770 <shelket17@gmail.com>
2026-09-05 00:20:06 +0530

    cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 
 const app = express();
@@ -8,6 +9,11 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}))
 
 app.use("/api/auth", authRouter);
 
