9e32c43f2eea2f6cce0b6b9434da698d5c6b63d7
Tejasshelke1770 <shelket17@gmail.com>
2026-09-06 16:28:01 +0530

    google auth middlewares added

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,36 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import authRouter from "./routes/auth.routes.js";
+import { config } from "./config/config.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
+app.use(passport.initialize());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.GOOGLE_CLIENT_ID,
+      clientSecret: config.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/api/auth/google/callback",
+    },
+    (_, __, profile, done) => {
+      return done(null, profile);
+    },
+  ),
+);
 
 app.use("/api/auth", authRouter);
 
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,9 +7,22 @@ if (!process.env.MONGO_URI) {
 if (!process.env.JWT_SECRET_KEY) {
   throw new Error("JWT_SECRET_KEY is not defined in environment variables");
 }
+if (
+  !process.env.GOOGLE_CLIENT_ID ||
+  !process.env.GOOGLE_CLIENT_SECRET ||
+  !process.env.GOOGLE_CALLBACK_URI
+) {
+  throw new Error(
+    "GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET is not defined in environment variables",
+  );
+}
 
 export const config = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URI: process.env.GOOGLE_CALLBACK_URI,
+  NODE_ENV: process.env.NODE_ENV || "development",
 };
