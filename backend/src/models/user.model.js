cd23492c247067a34fc0aeeec14fed59d45e336a
Tejasshelke1770 <shelket17@gmail.com>
2026-09-15 02:24:45 +0530

    model name updated

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -44,6 +44,6 @@ userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
-const userModel = mongoose.model("users", userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 export default userModel;
