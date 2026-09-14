7c9603774ece42d5420af6e78220328afb341da4
Tejasshelke1770 <shelket17@gmail.com>
2026-09-15 04:33:37 +0530

    fixes

--- backend/src/validators/cart.validator.js
+++ backend/src/validators/cart.validator.js
@@ -1,7 +1,7 @@
 import { body, param, validationResult } from "express-validator";
 
 const validateRequest = (req, res, next) => {
-  const errors = validationResult();
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({
       errors: errors.array(),
