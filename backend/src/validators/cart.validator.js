c94407067ea7a3f5d13ba70ba3c6eb6d703202f7
Tejasshelke1770 <shelket17@gmail.com>
2026-09-15 02:22:46 +0530

    cart validator created

---
+++ backend/src/validators/cart.validator.js
@@ -0,0 +1,21 @@
+import { body, param, validationResult } from "express-validator";
+
+const validateRequest = (req, res, next) => {
+  const errors = validationResult();
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+  next();
+};
+
+export const validateCart = [
+  param("productId").isMongoId().withMessage("invalid product ID"),
+  param("variantId").optional().isMongoId().withMessage("invalid variant ID"),
+  body("quantity")
+    .optional()
+    .isInt({ min: 1 })
+    .withMessage("Quantity atleast must be 1"),
+  validateRequest,
+];
